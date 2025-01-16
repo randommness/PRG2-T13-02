8aed709eda6f0f4a14d384e1643a9aa6f4a52fa9
Wen Jye Timothy Sim <s10240903@connect.np.edu.sg>
2025-01-16 14:31:35 +0800

    try commit class

---
+++ PRG2-T13-02/Flight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_T13_02
+{
+    internal abstract class Flight
+    {
+        public string FlightNumber { get; set; }
+        public string origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime ETD { get; set; }
+        public DateTime ETA { get; set; }
+        public Flight(string flightNumber, string destination, DateTime etd, DateTime eta)
+        {
+            FlightNumber = flightNumber;
+            Destination = destination;
+            ETD = etd;
+            ETA = eta;
+        }
+        public abstract double CalculatePrice();
+        public abstract string ToString();
+    }
+}
